5159a5f858041d91fe0486ad07ec0a388e6b38b7
aparnasj <appu.2410@gmail.com>
2020-06-13 20:03:33 +0530

    integrated quilljs and giphy with bootstrap model

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var giph_api_key = 'nlV1KCUTn1bwI9H6j5GQV2RvYfdETWdc'; // Not a good practice.. Should be moved to server and have proxy APIs
 var random_id = Math.floor(Math.random() * 1000000000);
 
-function fetchTrendingGifs(page) {
+function fetchTrendingGifs(page, isModal) {
   $.ajax({
     method: 'GET',
     url:
@@ -14,18 +14,25 @@ function fetchTrendingGifs(page) {
     var generatedItems = '';
     for (let i = 0; i < response.data.length; i++) {
       generatedItems +=
-        '<div class="col-3 giphy-wrapper"> <div class="giphy-item"> <img src="' +
+        '<div class="col-3 giphy-wrapper"> <div class="giphy-item" ' +
+        (isModal
+          ? 'onclick="selectGiphy(\'' +
+            response.data[i].images.downsized.url +
+            '\')"'
+          : '') +
+        ' > <img src="' +
         response.data[i].images.downsized.url +
         '"> </div> </div>';
     }
-    if (!page) $('#giphy-images').html(generatedItems);
-    else $('#giphy-images').append(generatedItems);
+    if (!page)
+      $('#giphy-images' + (isModal ? 'modal' : '')).html(generatedItems);
+    else $('#giphy-images' + (isModal ? 'modal' : '')).append(generatedItems);
     window.nextPage = page ? page++ : 2;
   });
 }
 
-function searchGiphyImages(page) {
-  var q = $('#giphy-search-input').val();
+function searchGiphyImages(page, isModal) {
+  var q = $('#giphy-search-input' + (isModal ? 'modal' : '')).val();
   $.ajax({
     method: 'GET',
     url:
@@ -44,12 +51,19 @@ function searchGiphyImages(page) {
       : '';
     for (let i = 0; i < response.data.length; i++) {
       generatedItems +=
-        '<div class="col-3 giphy-wrapper"> <div class="giphy-item"> <img src="' +
+        '<div class="col-3 giphy-wrapper"> <div class="giphy-item" ' +
+        (isModal
+          ? 'onclick="selectGiphy(\'' +
+            response.data[i].images.downsized.url +
+            '\')"'
+          : '') +
+        '> <img src="' +
         response.data[i].images.downsized.url +
         '"> </div> </div>';
     }
-    if (!page) $('#giphy-images').html(generatedItems);
-    else $('#giphy-images').append(generatedItems);
+    if (!page)
+      $('#giphy-images' + (isModal ? 'modal' : '')).html(generatedItems);
+    else $('#giphy-images' + (isModal ? 'modal' : '')).append(generatedItems);
     window.nextPage = page ? page++ : 1;
   });
 }
@@ -67,11 +81,29 @@ function loadMoreGifs() {
   }
 }
 
+function selectGiphy(url) {
+  window.quillEditorWithGiphy.insertEmbed(
+    window.quillEditorWithGiphy.scroll.length(),
+    'image',
+    url
+  );
+  $('#giphyModal').modal('hide');
+}
+
 $(document).ready(function () {
   fetchTrendingGifs();
-  var options = {
+  window.quillEditor = new Quill('#quill-editor', {
     modules: { toolbar: '#toolbar' },
     theme: 'snow',
-  };
-  window.quillEditor = new Quill('#quill-editor', options);
+  });
+
+  window.quillEditorWithGiphy = new Quill('#quill-editor-withgiphy', {
+    modules: { toolbar: '#toolbar-withgiphy' },
+    theme: 'snow',
+  });
+
+  $('#giphyModal').on('show.bs.modal', function (e) {
+    $('#giphy-search-inputmodal').val('');
+    fetchTrendingGifs(null, true);
+  });
 });
